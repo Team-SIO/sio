ac14c0c896c1fff1d296124065a4f0bec44769c6
satoshi ono <saongtx7@gmail.com>
2018-10-12 23:26:48 +0900

    Add timezone.etc config

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,16 @@ module Sio
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    config.generators do |g|
+      g.test_framework  nil 
+      g.assets  false
+      g.helper false
+      g.stylesheets false
+    end    
+    config.active_record.default_timezone = :local
+    config.i18n.available_locales = [:en, :ja]
+    config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,7 +8,7 @@ Rails.application.configure do
 
   # Do not eager load code on boot.
   config.eager_load = false
-
+  config.logger = Logger.new("log/development.log", 5, 5 * 1024 * 1024)
   # Show full error reports.
   config.consider_all_requests_local = true
 
