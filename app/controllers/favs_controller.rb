b118824763df0e3bf33dd107b4a4bf42eb38e58f
satoshi ono <saongtx7@gmail.com>
2018-11-17 13:29:35 +0900

    いいね非同期

--- app/controllers/favs_controller.rb
+++ app/controllers/favs_controller.rb
@@ -10,14 +10,12 @@ class FavsController < ApplicationController
   def destroy
     fav = Fav.find_by(user_id: current_user.id, item_id: params[:item_id])
     fav.destroy
-    @favs = fav.where(item_id: params[:item_id])
+    @favs = Fav.where(item_id: params[:item_id])
     @item.reload
   end
 
   private
-
   def set_item
     @item = Item.find(params[:item_id])
   end
-
 end
